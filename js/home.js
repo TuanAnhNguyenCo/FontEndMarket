8bfa508fabe33c7af82efaed753e7ca1e3a63791
Nguyen Co Tuan Anh <coanh2710@gmail.com>
2022-07-10 21:23:18 +0700

    Edit form

--- js/home.js
+++ js/home.js
@@ -1,9 +1,4 @@
-var api = "https://c3e9-58-187-157-221.ap.ngrok.io";
-console.log(localStorage.getItem("token"))
-
-var headers = {"Authorization" :`Bearer ${localStorage.getItem("token")}`}
-console.log(headers)
-
+var api = "https://ee30-58-187-157-221.ap.ngrok.io";
 
 
 $(document).ready(function() {
@@ -19,10 +14,8 @@ $(document).ready(function() {
             password: $("#password").val()
         },
         success: function (data) {
-          window.location.replace("http://127.0.0.1:8088/web/home.html");
           window.localStorage.setItem('token', data['jwt'])
-         
-         
+          window.location.replace("http://127.0.0.1:8088/web/home.html");
       },
       error: function (e) {
          alert("Error")
@@ -32,37 +25,3 @@ $(document).ready(function() {
   });
 });
 
-
-
-
-
-
-// if (localStorage.getItem('token')!=NaN)
-//   var token = localStorage.getItem('token')
-
-// var obj = {
-//     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Authorization':`Bearer ${token}`,
-//     },
-// }
-
-// fetch(api+"/api/v1/home",obj)
-//   .then(function(response) {
-//         return response.json()
-//   })
-//   .then(function(myJSON) {
-//         myJSON = myJSON[0]
-//         var img = myJSON['image'].split(" ")
-//         function Showdata(data){
-//             content.innerHTML += "<img src="+api+data+ " alt='Lỗi'>"; 
-//             // content.innerText = "sdasd"
-//             console.log("<img src="+api+data+ " alt='Lỗi'>")
-//         } 
-//         img.forEach(Showdata)
-//   })
-//   .catch(function(error)  {
-//       console.log("Noooooo! Something error:");
-//       console.log(error);
-//   });
